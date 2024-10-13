7bc46570f852e124c9eceb6b1a9e7575dfa5df7a
rob <rob@syn-2603-8080-1806-fff3-89fb-3f0e-4869-99a0.res6.spectrum.com>
2024-10-12 20:41:40 -0600

    John Question_C

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,1 +1,12 @@
-#add import
+import question_c
+
+def main():
+    to_continue = True
+    while (to_continue):
+        day = int(input("Enter a number for the day of the week "))
+        print(question_c.get_day_of_week(day))
+        to_quit_or_not_to_quit = input("Do you want to quit? [y/n] ")
+        if (to_quit_or_not_to_quit == "y" or to_quit_or_not_to_quit == "Y"):
+            to_continue = False
+
+main()
--- tests/question_tests/question_tests.py
+++ tests/question_tests/question_tests.py
@@ -2,9 +2,13 @@
 import unittest
 
 #follow this example to add questions b, c, and d for testing including their functions
-from src.question_b.question_b import get_miles_per_hour
+from src.question_c.question_c import get_day_of_week
 
 class Test_Config(unittest.TestCase):
 
-    def test_get_miles_per_hour(self):
-        self.assertEqual(19.883872, get_miles_per_hour(32, 60))
+    def test_get_day_of_week(self):
+        self.assertEqual("Invalid number", get_day_of_week(0))
+        self.assertEqual("Monday", get_day_of_week(1))
+        self.assertEqual("Tuesday", get_day_of_week(2))
+        self.assertEqual("Wednesday", get_day_of_week(3))
+        self.assertEqual("Invalid number", get_day_of_week(8))
