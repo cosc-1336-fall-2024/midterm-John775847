9191e7a8f6ff6254e96c8d383bf2e54767e8b9b1
rob <rob@syn-2603-8080-1806-fff3-89fb-3f0e-4869-99a0.res6.spectrum.com>
2024-10-12 19:35:08 -0600

    John Question_A

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,8 @@
-#add import
+import question_a
+
+def main():
+    actual_value = int(input("Enter the actual value of a piece of property "))
+    print("Assessment value", question_a.get_assessment_value(actual_value))
+    print("Tax assessed", question_a.get_tax_assessed(question_a.get_assessment_value(actual_value)))
+
+main()
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,8 @@
 #write functions here, don't add input('') statements here!
-def test_config():
-    return True
+
+def get_assessment_value(property_value):
+    return property_value * 0.6
+
+def get_tax_assessed(acre_assesment):
+    return round((acre_assesment/100)*0.72, 2)
+    #Round to 2 digits because 100 ¢ in a $.
